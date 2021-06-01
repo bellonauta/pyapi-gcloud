db1ab0086089cfabd7a9b645fde0d1622fd82998
belonauta@hotmail.com <wilsom.angeli@gmail.com>
2021-06-01 12:13:41 +0000

    Implementação

--- py_api_consts.py
+++ py_api_consts.py
@@ -4,27 +4,6 @@
 
 import json
 
-# Ambiente...
-_IN_PRODUCTION = False
-
-# Constantes de conexão com o banco de dados...
-_PG_CONNECTION = {
-                    'devel': {
-                       'host': 'localhost',
-                       'port': 5432,
-                       'name': 'pyapi',
-                       'user': 'postgres',
-                        'pwd': 'postgres'
-                    },
-                    'production': {
-                       'host': '35.199.98.84', # Gcloud
-                       'port': 5432,
-                       'name': 'gcloud',
-                       'user': 'postgres',
-                        'pwd': '03*1966pggc'
-                    }
-}                    
-
 # Constantes de estado/situação/atividade...
 _YES = 'y'
 _NO = 'n'
